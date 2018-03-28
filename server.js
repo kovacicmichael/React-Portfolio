9e3e813e1fe70022500ed4944854cde713ee6c69
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-03-28 11:04:56 -0600

    react setup complete

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require ("./controllers/apiRoutes")
 // Require all models
 var db = require("./models");
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3001;
 
 // Initialize Express
 var app = express();
