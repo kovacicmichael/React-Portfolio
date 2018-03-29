9ce5a6e4f1152ea2560532be3e2f4e914003935f
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-03-29 14:02:21 -0600

    new modal working

---
+++ client/public/js/src.js
@@ -0,0 +1,15 @@
+
+
+console.log("here in src.js")
+
+
+$('.button').click(function(){
+  var buttonId = $(this).attr('id');
+  $('#modal-container').removeAttr('class').addClass(buttonId);
+  $('body').addClass('modal-active');
+})
+
+$('#modal-container').click(function(){
+  $(this).addClass('out');
+  $('body').removeClass('modal-active');
+});
--- client/src/components/Skills/Skills.js
+++ client/src/components/Skills/Skills.js
@@ -33,10 +33,63 @@ componentDidMount() {
 				<div className="container containerSkills">
 					<Col md={12}>
 							<div class="skillTitle">
-								Areas of Expertise
+								Services
 							</div>
 					</Col>
-						<Col xs={12} sm={8} smOffset={2}>
+					<Col md={12}>
+							<div class="offerTitle">
+								What I Can Offer
+							</div>
+					</Col>
+						<Col md={3}>
+							
+							<h3>BackEnd</h3>
+							<h4>Node.js</h4>
+							<ul>
+								<li>Express.js</li>
+								<li>User Authentication</li>
+								<li>MERN Stack</li>				
+							</ul>
+							<h4>Databases</h4>
+							<ul>
+								<li>MySQL</li>
+								<li>Mongo</li>
+								<li>Sequelize</li>
+								<li>Mongoose</li>				
+							</ul>
+						</Col>
+						<Col md={3}>
+							
+							<h3>FrontEnd</h3>
+							<h4></h4>
+							<ul>
+								<li>Express.js</li>
+								<li>User Authentication</li>
+								<li>MERN Stack</li>				
+							</ul>
+							<h4>Databases</h4>
+							<ul>
+								<li>MySQL</li>
+								<li>Mongo</li>
+								<li>Sequelize</li>
+								<li>Mongoose</li>				
+							</ul>
+						</Col>
+						<Col md={3}>
+							
+							<h3>{this.state.aboutName}</h3>
+							<p>{this.state.aboutBio}</p>
+							<p>						
+								Suas omnium eu mel. Veri libris vix in, his indoctum definitiones ex, ubique corpora duo ex. Cum error lobortis at, nemore laboramus mei id. Putant inimicus id sit. Habeo mandamus reprimique sed ei, consul volumus corrumpit cum in.
+
+								Tollit veritus invidunt his te, mei ea suas graeco. Quando euismod id mea, mel ne omnis vituperatoribus, tation ubique vel an. Eos hendrerit vituperatoribus an, usu ut summo voluptatibus. Has ubique deleniti ei, vitae epicurei eum et. Modo ullum ridens an has, lorem consectetuer duo ne, in iusto splendide disputando usu.
+
+								Velit dissentias no sit. Nec lorem tation facete ea, an his expetendis intellegebat. Mei ei lorem commune officiis, pro mucius principes eu. Te modus indoctum nec. At vim oblique repudiare assentior. Pri quem inciderint cu, iudico labore eam eu, viderer consequat complectitur usu ne.
+
+								Modus detraxit pro in, aliquip assentior his ne, id prompta philosophia ius. Usu postea admodum expetendis te. Movet euismod vocibus vix ut. Mea ad nostro voluptua nominati, vix ceteros fastidii eloquentiam ut, quis ornatus eos et. Elitr facete aliquam ad his, alii errem eum cu. Ut dignissim argumentum duo, te facilisi assueverit sadipscing per. Ubique tamquam incorrupte his an.
+							</p>
+						</Col>
+						<Col md={3}>
 							
 							<h3>{this.state.aboutName}</h3>
 							<p>{this.state.aboutBio}</p>
