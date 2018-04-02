3ceaad324b7bf3f34c4cdabf56c840765c6b91a1
Robert Gerboth <email@example.com>
2018-04-02 12:53:59 -0600

    Added project update function on Admin

--- client/src/components/UpdateBtn/UpdateBtn.js
+++ client/src/components/UpdateBtn/UpdateBtn.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "./DeleteBtn.css";
+import "./UpdateBtn.css";
 
 // The ...props means, spread all of the passed props onto this element
 // That way we don't have to define them all individually
-const DeleteBtn = props => (
-  <span className="delete-btn" {...props}>
-    DELETE
+const UpdateBtn = props => (
+  <span className="update-btn" {...props}>
+    UPDATE
   </span>
 );
 
-export default DeleteBtn;
+export default UpdateBtn;
--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -12,258 +12,174 @@ module.exports = function(app){
 
   console.log("exporting properly")
 
+  // Route for getting all Portfolio items and profile information from the db
+  app.get("/api/renderPage", function(req, res) {
 
+    console.log("here in the renderPage route")
 
-    // Route for getting all Portfolio items and profile information from the db
-    app.get("/api/renderPage", function(req, res) {
-
-      console.log("here in the renderPage route")
-  
-      let promiseArray = [
-        db.Portfolio.find({}),
-        db.About.find({}),
-        db.Contact.find({})
-      ];
-
-      Promise.all(promiseArray).then(function(values) {
-
-        console.log(values)
-          res.json({
-            portfolio: values[0],
-            about: values[1],
-            contacts: values[2]
-          });
-        console.log("we are hereeeeee")
-
-        }).catch(function(err) {
-
-          res.json(err);
-        });
-
-    });
-
-
-
-    //Route for grabbing a specific Portfolio item by id, populate it with it's note
-    //do not comment this out!
-    app.get("/portfolioModal/:id", function(req, res) {
-
-      db.Portfolio.findOne({ _id: req.params.id })
-        // .populate("note")  // do we need to populate anything on the portfolio? 
-        .then(function(dbPortfolio) {
-
-          res.json(dbPortfolio);
-        })
-        .catch(function(err) {
-
-          res.json(err);
-        });
-    });
-
-    //Route for getting all contacts
-    app.get("/contacts", function(req, res) {
-
+    let promiseArray = [
+      db.Portfolio.find({}),
+      db.About.find({}),
       db.Contact.find({})
-        .then(function(dbContacts){
+    ];
 
-          res.json(dbContacts);
+    Promise.all(promiseArray).then(function(values) {
 
-        })
-        .catch(function(err) {
-
-          res.json(err);
+      console.log(values)
+        res.json({
+          portfolio: values[0],
+          about: values[1],
+          contacts: values[2]
         });
-    });
+      console.log("we are hereeeeee")
 
-        // Route for deleting an existing contact 
-    app.delete("/contacts/:id", function(req, res) {
+      }).catch(function(err) {
 
-      var contactID = req.params.id
+        res.json(err);
+      });
 
-      db.Contact.remove({ _id: contactID })
-        .then(function(dbContact) {
-          console.log("Deleted Contact ID: " + contactID)
-        })
-        .then(function(dbContact) {
-          res.json(dbContact);
-        })
-        .catch(function(err) {
-          // If an error occurs, send it back to the client
-          res.json(err);
-        });
-    });
+  });
 
-    // Route for getting Home page info
-    // app.get("/home", function(req, res) {
+  //Route for grabbing a specific Portfolio item by id, populate it with it's note
+  //do not comment this out!
+  app.get("/portfolioModal/:id", function(req, res) {
 
-    //   db.HomePage.find({})
-    //     .then(function(dbHome){
+    db.Portfolio.findOne({ _id: req.params.id })
+      // .populate("note")  // do we need to populate anything on the portfolio? 
+      .then(function(dbPortfolio) {
 
-    //       res.json(dbHome);
+        res.json(dbPortfolio);
+      })
+      .catch(function(err) {
 
-    //     })
-    //     .catch(function(err) {
+        res.json(err);
+      });
+  });
 
-    //       res.json(err);
-    //     });
-    // });
+  //Route for getting all contacts
+  app.get("/contacts", function(req, res) {
 
-    //Route for getting About page
-    app.get("/about", function(req, res) {
+    db.Contact.find({})
+      .then(function(dbContacts){
 
-      db.About.find({})
-        .then(function(dbStats) {
+        res.json(dbContacts);
 
-          res.json(dbStats);
+      })
+      .catch(function(err) {
 
-        })
-        .catch(function(err) {
+        res.json(err);
+      });
+  });
 
-          res.json(err);
-        });
-    });
+      // Route for deleting an existing contact 
+  app.delete("/contacts/:id", function(req, res) {
 
-    //Route for getting page stats
-    app.get("/stats", function(req, res) {
+    var contactID = req.params.id
 
-      db.PageStats.find({})
-        .then(function(dbStats) {
+    db.Contact.remove({ _id: contactID })
+      .then(function(dbContact) {
+        console.log("Deleted Contact ID: " + contactID)
+      })
+      .then(function(dbContact) {
+        res.json(dbContact);
+      })
+      .catch(function(err) {
+        // If an error occurs, send it back to the client
+        res.json(err);
+      });
+  });
 
-          res.json(dbStats);
+  //Route for getting page stats
+  app.get("/stats", function(req, res) {
 
-        })
-        .catch(function(err) {
+    db.PageStats.find({})
+      .then(function(dbStats) {
 
-          res.json(err);
-        });
-    });
+        res.json(dbStats);
 
-    // Route for updating an existing portfolio 
-    app.post("/portfolio/:id", function(req, res) {
+      })
+      .catch(function(err) {
 
-      var portfolioID = req.params.id
+        res.json(err);
+      });
+  });
 
-      db.Portfolio.findOneAndUpdate(req.body)
-        .then(function(dbPortfolio) {
-          console.log("Updated Portfolio ID: " + dbPortfolio._id)
-        })
-        .then(function(dbPortfolio) {
-          // If the Article was updated successfully, send it back to the client
-          res.json(dbPortfolio);
-        })
-        .catch(function(err) {
-          // If an error occurs, send it back to the client
-          res.json(err);
-        });
-    });
+  // Route for updating an existing portfolio 
+  app.post("/portfolio/:id", function(req, res) {
 
-    // Route for deleting an existing portfolio 
-    app.delete("/portfolio/:id", function(req, res) {
+    var portfolioID = req.params.id
 
-      var portfolioID = req.params.id
+    console.log(`Portfolio ID to Update: ${portfolioID}`);
+    console.log(req.body);
 
-      db.Portfolio.remove({ _id: portfolioID })
-        .then(function(dbPortfolio) {
-          console.log("Deleted Portfolio ID: " + portfolioID)
-        })
-        .then(function(dbPortfolio) {
-          res.json(dbPortfolio);
-        })
-        .catch(function(err) {
-          // If an error occurs, send it back to the client
-          res.json(err);
-        });
-    });
+    db.Portfolio.findOneAndUpdate({ _id: portfolioID}, req.body)
+      .then(function(dbPortfolio) {
+        console.log("Updated Portfolio ID: " + dbPortfolio._id)
+      })
+      .then(function(dbPortfolio) {
+        // If the Article was updated successfully, send it back to the client
+        res.json(dbPortfolio);
+      })
+      .catch(function(err) {
+        // If an error occurs, send it back to the client
+        res.json(err);
+      });
+  });
 
-    // Route for adding a new portfolio project 
-    app.post("/portfolio", function(req, res) {
-      console.log(`Inside the /portfolio POST route.`)
-      console.log(req.body)
-      db.Portfolio.create(req.body)
-        .then(function(dbPortfolio) {
-          console.log("New Portfolio ID: " + dbPortfolio._id)
-        })
-        .then(function(dbPortfolio) {
+  // Route for deleting an existing portfolio 
+  app.delete("/portfolio/:id", function(req, res) {
 
-          res.json(dbPortfolio);
-        })
-        .catch(function(err) {
+    var portfolioID = req.params.id
 
-          res.json(err);
-        });
-    });
-    // Route for saving/updating an Article's associated Note
-    // app.post("/home", function(req, res) {
+    db.Portfolio.remove({ _id: portfolioID })
+      .then(function(dbPortfolio) {
+        console.log("Deleted Portfolio ID: " + portfolioID)
+      })
+      .then(function(dbPortfolio) {
+        res.json(dbPortfolio);
+      })
+      .catch(function(err) {
+        // If an error occurs, send it back to the client
+        res.json(err);
+      });
+  });
 
-    //   db.HomePage.create(req.body)
-    //     .then(function(dbPortfolio) {
-    //       console.log("New Portfolio ID: " + dbHome._id)
-    //     })
-    //     .then(function(dbPortfolio) {
-    //       // If the Article was updated successfully, send it back to the client
-    //       res.json(dbPortfolio);
-    //     })
-    //     .catch(function(err) {
-    //       // If an error occurs, send it back to the client
-    //       res.json(err);
-    //     });
-    // });
+  // Route for adding a new portfolio project 
+  app.post("/portfolio", function(req, res) {
+    console.log(`Inside the /portfolio POST route.`)
+    console.log(req.body)
+    db.Portfolio.create(req.body)
+      .then(function(dbPortfolio) {
+        console.log("New Portfolio ID: " + dbPortfolio._id)
+      })
+      .then(function(dbPortfolio) {
 
+        res.json(dbPortfolio);
+      })
+      .catch(function(err) {
 
-    // Route for saving/updating 
-    // app.post("/home/:ID", function(req, res) {
+        res.json(err);
+      });
+  });
 
-    //   var homeID = req.params.id
+  // Route for saving/updating About (bio) information
+  app.post("/about/:id", function(req, res) {
 
-    //   db.HomePage.update(req.body)
-    //     .then(function(dbPortfolio) {
-    //       console.log("New Portfolio ID: " + dbHome._id)
-    //     })
-    //     .then(function(dbPortfolio) {
-    //       // If the Article was updated successfully, send it back to the client
-    //       res.json(dbPortfolio);
-    //     })
-    //     .catch(function(err) {
-    //       // If an error occurs, send it back to the client
-    //       res.json(err);
-    //     });
-    // });
-
-    // Route for saving/updating an Article's associated Note
-    app.post("/about", function(req, res) {
-
-      db.About.create(req.body)
-        .then(function(dbPortfolio) {
-          console.log("New Portfolio ID: " + dbAbout._id)
-        })
-        .then(function(dbPortfolio) {
-          // If the Article was updated successfully, send it back to the client
-          res.json(dbPortfolio);
-        })
-        .catch(function(err) {
-          // If an error occurs, send it back to the client
-          res.json(err);
-        });
-    });
-
-    // Route for saving/updating About (bio) information
-    app.post("/about/:id", function(req, res) {
-
-      var aboutID = req.params.id
-      console.log(`aboutID: ${aboutID}`)
-      console.log(`Req.body: ${JSON.stringify(req.body)}`)
-      db.About.findOneAndUpdate({ _id: aboutID}, req.body)
-        .then(function(dbAbout) {
-          console.log("Updated About ID: " + dbAbout._id)
-        })
-        .then(function(dbAbout) {
-          // If the Article was updated successfully, send it back to the client
-          res.json(dbAbout);
-        })
-        .catch(function(err) {
-          // If an error occurs, send it back to the client
-          res.json(err);
-        });
-    });
+    var aboutID = req.params.id
+    console.log(`aboutID: ${aboutID}`)
+    console.log(`Req.body: ${JSON.stringify(req.body)}`)
+    db.About.findOneAndUpdate({ _id: aboutID}, req.body)
+      .then(function(dbAbout) {
+        console.log("Updated About ID: " + dbAbout._id)
+      })
+      .then(function(dbAbout) {
+        // If the Article was updated successfully, send it back to the client
+        res.json(dbAbout);
+      })
+      .catch(function(err) {
+        // If an error occurs, send it back to the client
+        res.json(err);
+      });
+  });
 
 }
