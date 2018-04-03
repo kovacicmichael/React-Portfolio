26da79257ab73ea4f9fe1fecaaca945d7c02b522
Michael Kovacic <michael.j.kovacic@gmail.com>
2018-04-03 12:33:52 -0600

    server

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("client/build"));
+//app.use(express.static("public"));
 
 
 
